de8aad09ed0eff97479922b270dd82cbd8948dc0
Xamel7 <joel.lewis47@yahoo.com>
2023-09-20 11:50:39 -0500

    Lab11 schema

--- WebApplication1/Controllers/UserController.cs
+++ WebApplication1/Controllers/UserController.cs
@@ -1,14 +1,139 @@
 ﻿using Lab12.Models;
 using Lab12.Models.Services;
-using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
 
 namespace Lab12.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController
+    public class UserController : ControllerBase
     {
+        private UserManager<ApplicationUser> userManager;
+        private SignInManager<ApplicationUser> signInManager;
+        private JwtToken tokenService;
 
+        public UserController(UserManager<ApplicationUser> manager, JwtToken _tokenService, SignInManager<ApplicationUser> _signInManager)
+        {
+            userManager = manager;
+            tokenService = _tokenService;
+            signInManager = _signInManager;
+        }
+
+        [HttpPost("Register")]
+        [AllowAnonymous]
+
+        public async Task<ActionResult<ApplicationUser>> Register(ApplicationUser user)
+        {
+            var result = await userManager.CreateAsync(user, user.Password);
+            if (result.Succeeded)
+            {
+                List<string> roles = new List<string>
+                {
+                    "Anonymous"
+                };
+                await signInManager.SignInAsync(user, null);
+
+                return new ApplicationUser
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Token = await tokenService.GetToken(user, System.TimeSpan.FromMinutes(15)),
+                    Roles = roles
+                };
+            }
+
+            foreach (var error in result.Errors)
+            {
+                var errorKey =
+
+                    error.Code.Contains("Password") ? nameof(user.Password) :
+                    error.Code.Contains("Email") ? nameof(user.Email) :
+                    error.Code.Contains("UserName") ? nameof(user.UserName) :
+                    "";
+                ModelState.AddModelError(errorKey, error.Description);
+            }
+
+            if (ModelState.IsValid)
+            {
+                return user;
+            }
+            return BadRequest(new ValidationProblemDetails(ModelState));
+        }
+
+        [Authorize]
+        [Authorize(Policy = "PropertyManager")]
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        [HttpPost]
+        [Route("Agent")]
+
+        public async Task<ActionResult<ApplicationUser>> CreateAgent(ApplicationUser user)
+        {
+            var result = await userManager.CreateAsync(user, user.Password);
+
+            if (result.Succeeded)
+            {
+                return new ApplicationUser
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Roles = new List<string> { "Agent" },
+                    Token = await tokenService.GetToken(user, System.TimeSpan.FromMinutes(15))
+                };
+            }
+
+            foreach (var error in result.Errors)
+            {
+                var errorKey =
+
+                   error.Code.Contains("Password") ? nameof(user.Password) :
+                   error.Code.Contains("Email") ? nameof(user.Email) :
+                   error.Code.Contains("UserName") ? nameof(user.UserName) :
+                   "";
+                ModelState.AddModelError(errorKey, error.Description);
+            }
+            return BadRequest(new ValidationProblemDetails(ModelState));
+        }
+
+        [HttpPost("Login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<ApplicationUser>> Login(ApplicationUser data)
+        {
+            var user = await userManager.FindByNameAsync(data.UserName);
+
+            if (await userManager.CheckPasswordAsync(user, data.Password))
+            {
+                await signInManager.SignInAsync(user, null);
+
+                return new ApplicationUser()
+                {
+                    Id = user.Id,
+                    Token = await tokenService.GetToken(user, System.TimeSpan.FromMinutes(15)),
+                    Roles = user.Roles
+                };
+            }
+            if(user == null)
+            {
+                return Unauthorized();
+            }
+            return BadRequest();
+        }
+
+        [Authorize(Policy = "create")]
+        [HttpGet("me")]
+        public async Task<ApplicationUser> GetUser(ClaimsPrincipal principal)
+        {
+            var user = await userManager.GetUserAsync(principal);
+
+            return new ApplicationUser
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Roles = await userManager.GetRolesAsync(user),
+            };
+        }
     }
 }
---
+++ WebApplication1/README.md
@@ -0,0 +1,62 @@
+Async Inn Hotel
+ERD Components
+Hotel (Table)
+
+Primary Key: HotelID
+Name
+City
+State
+Address
+PhoneNumber
+Room (Table)
+
+Primary Key: RoomID
+Foreign Key: RoomLayoutID (links to the RoomLayout table for the layout type)
+Name (e.g., "Seahawks Snooze")
+PetFriendly (boolean to indicate if the room is pet-friendly)
+RoomLayout (Enum/Table)
+
+Primary Key: LayoutID
+LayoutName (e.g., "One bedroom", "Two bedrooms", "Studio")
+Amenity (Table)
+
+Primary Key: AmenityID
+Name (e.g., "air conditioning", "coffee maker")
+RoomAmenity (Pure Join Table)
+
+Composite Key: RoomID + AmenityID
+Foreign Key: RoomID (links to Room)
+Foreign Key: AmenityID (links to Amenity)
+HotelRoom (Joint Entity Table with Payload)
+
+Composite Key: HotelID + RoomID
+Foreign Key: HotelID (links to Hotel)
+Foreign Key: RoomID (links to Room)
+Price (price varies per location and per room number)
+RoomNumber
+Relationships
+A Hotel can have many HotelRooms, so it's a one-to-many relationship between Hotel and HotelRoom.
+A Room can have many Amenities and an Amenity can belong to many rooms, forming a many-to-many relationship. This is represented through the RoomAmenity table.
+A Room has one RoomLayout and a RoomLayout can be associated with many rooms, forming a one-to-many relationship.
+Explanations:
+Hotel:
+
+Represents each hotel location with its respective details. Each hotel is unique with its own ID.
+Room:
+
+Represents the different room types and names. The layout information is stored in a separate table, and the two are connected via a foreign key.
+RoomLayout:
+
+An enum or table that contains the different layout configurations a room can have.
+Amenity:
+
+Represents the different amenities that can be available in a room. These amenities can be shared across rooms.
+RoomAmenity:
+
+A join table that associates rooms with their respective amenities, since one room can have many amenities and vice versa.
+HotelRoom:
+
+Represents the specific rooms within a hotel. The price and room number are included here because they vary per hotel location.
+This ERD structure ensures normalization of the database and maintains the relationships between different tables efficiently. It can be expanded or modified as per future requirements.
+
+![image]("C:\Users\Student-16\Pictures\Schema.png")
